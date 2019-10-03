ad30abf52d340cb319a91100b829532fa2e71654
Thomas Johander <tom.johander@dealerteamwork.com>
2019-10-03 07:03:56 -0500

    Implemented basic JPA Repository

---
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -0,0 +1,10 @@
+package com.guitar.db.repository;
+
+import com.guitar.db.model.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class LocationJpaRespository extends JpaRepository<Location, Long> {
+
+  public findAll();
+
+}
