e79a57366e9a0548f298d2b5efbc8ab27a30b865
Thomas Johander <tom.johander@dealerteamwork.com>
2019-10-10 07:18:14 -0500

    First custom Jpa repository method is working, tests passing

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -1,11 +1,13 @@
 package com.guitar.db.repository;
 
 import com.guitar.db.model.Location;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
 
+  List<Location> findByStateLike(String stateName);
 
 }
